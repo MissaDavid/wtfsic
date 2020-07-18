dad05fb739e4db6c4613d41b74e143a5b4c2748e
MissaDavid <mei.chohee@gmail.com>
2020-07-18 13:54:04 +1200

    use hook to fetch recipes from backend api

--- wtfsic/src/App.js
+++ wtfsic/src/App.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class Recipe extends React.Component {
-  render() {
-    return <div>"hello"</div>;
-  }
-}
+import RecipeLister from './components/Recipe/RecipeLister';
 
 function App() {
+  const [recipeList, setRecipeList] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      const apiUrl = '/api/recipes';
+      try {
+        const response = await fetch(apiUrl);
+        const jsonData = await response.json();
+        console.log(jsonData);
+        setRecipeList(jsonData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchRecipes();
+  }, []);
+
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
       </header>
-      <body>
-        <Recipe />
-      </body>
+      <>
+        <RecipeLister recipes={recipeList} />
+      </>
     </div>
   );
 }
