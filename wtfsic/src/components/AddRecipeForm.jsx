dfb1105ea7040e2ec65bd1a8e0f3c51b746ed58a
MissaDavid <mei.chohee@gmail.com>
2020-09-30 12:08:05 +1300

    Use Fetch to post very basic recipe

--- wtfsic/src/components/AddRecipeForm.jsx
+++ wtfsic/src/components/AddRecipeForm.jsx
@@ -1,27 +1,53 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React, {useState} from 'react';
+import {useForm} from 'react-hook-form';
 
+async function postData(url = '', data = {}) {
+    const response = await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    return response.json()
+}
+
+const cleanRecipeData = (formData) => {
+     // TODO : check Django session to get user creds
+    // https://stackoverflow.com/questions/59524305/how-to-access-django-session-in-react
+        formData.author = 1
+        formData.ingredients = []
+        formData.cook_time = 30
+        formData.prep_time = 10
+    return formData
+}
 const AddRecipeForm = () => {
-  const { register, handleSubmit } = useForm();
-  const addRecipe = (data) => {
-    console.log(data);
-  };
-  return (
-    <form className='recipeForm' onSubmit={handleSubmit(addRecipe)}>
-      <h2>New Recipe</h2>
+    const {register, handleSubmit, errors} = useForm();
+    // const [unit, setUnit] = useState('unit')
 
-      <label htmlFor='title'>Title</label>
-      <input name='title' ref={register({ required: true })} />
+    const addRecipe = async (data) => {
+        const cleanData = cleanRecipeData(data)
+        const response = await postData('/api/recipes', cleanData)
+        console.log(response)
+    };
+    
+    return (
+        <form className='recipeForm' onSubmit={handleSubmit(addRecipe)}>
+            <h2>New Recipe</h2>
 
-      <label htmlFor='description'>Description</label>
-      <input name='description' ref={register} />
+            <label htmlFor='title'>Title</label>
+            <input name='title' ref={register({required: true})}/>
 
-      <label htmlFor='instructions'>Step by step instructions</label>
-      <textarea name='instructions' ref={register({ required: true })} />
+            <label htmlFor='description'>Description</label>
+            <input name='description' ref={register}/>
 
-      <input className='submit-button' type='submit' />
-    </form>
-  );
+            <label htmlFor='instructions'>Step by step instructions</label>
+            <textarea name='instructions' ref={register({required: true})}/>
+
+            <input className='submit-button' type='submit'/>
+        </form>
+    );
 };
 
 export default AddRecipeForm;
