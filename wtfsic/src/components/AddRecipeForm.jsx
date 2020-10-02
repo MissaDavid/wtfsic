aace03da546069ec9f8825ae41a156c357d58510
MissaDavid <mei.chohee@gmail.com>
2020-10-02 15:57:59 +1300

    Moving addRecipe function out of the form and quick refactor

--- wtfsic/src/components/AddRecipeForm.jsx
+++ wtfsic/src/components/AddRecipeForm.jsx
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {useForm} from 'react-hook-form';
 
-async function postData(url = '', data = {}) {
+const postData = async (url = '', data = {}) => {
     const response = await fetch(url, {
         method: 'POST',
         mode: 'cors',
@@ -13,25 +13,26 @@ async function postData(url = '', data = {}) {
     return response.json()
 }
 
+// Those hard coded values are only for testing purposes
 const cleanRecipeData = (formData) => {
      // TODO : check Django session to get user creds
     // https://stackoverflow.com/questions/59524305/how-to-access-django-session-in-react
         formData.author = 1
         formData.ingredients = []
-        formData.cook_time = 30
-        formData.prep_time = 10
+        formData.cook_time = '00:30:00'
+        formData.prep_time = '00:10:00'
     return formData
 }
-const AddRecipeForm = () => {
-    const {register, handleSubmit, errors} = useForm();
-    // const [unit, setUnit] = useState('unit')
 
-    const addRecipe = async (data) => {
-        const cleanData = cleanRecipeData(data)
-        const response = await postData('/api/recipes', cleanData)
-        console.log(response)
-    };
-    
+ const addRecipe = async (data) => {
+    const cleanData = cleanRecipeData(data)
+     return await postData('/api/recipes', cleanData)
+}
+
+
+const AddRecipeForm = () => {
+    const {register, handleSubmit } = useForm();
+
     return (
         <form className='recipeForm' onSubmit={handleSubmit(addRecipe)}>
             <h2>New Recipe</h2>
