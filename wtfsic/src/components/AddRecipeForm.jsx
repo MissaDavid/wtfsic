3105b0259b3399e0d9ae5147b74e47d021b4f95d
thomasin <thomasinabraham@me.com>
2020-10-25 23:38:09 +0100

    Style form, allow for No! ingredients!
    Moved styling into its own file. Created a generic .table class for
    some normalisation styles, along with making some styles slightly
    more consistent (the inputs/buttons). Decided to leave it all in the
    one file for now. Decided to get make a table, get rid of it if no
    fields exist, with a simple "Ingredients" title. I played around with
    a few different ways of presenting this, and this one made the most
    sense.
    
    Allowed for no ingredients to be passed through. React-hook-form gets
    rid of the whole key if no ingredients exist. Decided not to make the
    field optional on the server, it feels more appropriate to add on the
    client. Re-appropriate the cleanData function as it is the simplest
    way of doing what we want! Can slowly clean up the test data and turn
    into a nice little helper function.

--- wtfsic/src/components/AddRecipeForm.jsx
+++ wtfsic/src/components/AddRecipeForm.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useForm, useFieldArray } from 'react-hook-form';
 
+import './AddRecipeForm.style.css';
+
 const postData = async (url = '', data = {}) => {
     const response = await fetch(url, {
         method: 'POST',
@@ -10,22 +12,24 @@ const postData = async (url = '', data = {}) => {
         },
         body: JSON.stringify(data)
     });
-    return response.json()
+    return response.json();
 }
 
-// Those hard coded values are only for testing purposes
 const cleanRecipeData = (formData) => {
+    formData.ingredients = formData.ingredients || [];
+
+    // Those hard coded values are only for testing purposes
     // TODO : check Django session to get user creds
     // https://stackoverflow.com/questions/59524305/how-to-access-django-session-in-react
-    formData.author = 1
-    formData.cook_time = '00:30:00'
-    formData.prep_time = '00:10:00'
-    return formData
+    formData.author = 1;
+    formData.cook_time = '00:30:00';
+    formData.prep_time = '00:10:00';
+    return formData;
 }
 
 const addRecipe = async (data) => {
-    const cleanData = cleanRecipeData(data)
-    return await postData('/api/recipes', cleanData)
+    const cleanData = cleanRecipeData(data);
+    return await postData('/api/recipes', cleanData);
 }
 
 const AddRecipeForm = () => {
@@ -35,8 +39,7 @@ const AddRecipeForm = () => {
         name: 'ingredients'
     });
 
-
-    const [units, setUnits] = useState()
+    const [units, setUnits] = useState(['units']);
     useEffect(() => {
         const fetchUnits = async () => {
           try {
@@ -51,10 +54,10 @@ const AddRecipeForm = () => {
         };
 
         fetchUnits();
-    }, [])
+    }, []);
 
     return (
-        <form className='recipeForm' onSubmit={handleSubmit(addRecipe)}>
+        <form className='recipe-form' onSubmit={handleSubmit(addRecipe)}>
             <h2>New Recipe</h2>
 
             <label htmlFor='title'>Title</label>
@@ -63,58 +66,79 @@ const AddRecipeForm = () => {
             <label htmlFor='description'>Description</label>
             <input name='description' ref={register}/>
 
-            <fieldset>
-                <legend>Ingredients</legend>
-                <ul>
-                    {fields.map((ingredient, index) => (
-                      <li key={ingredient.id}>
-                        <input
-                          name={`ingredients[${index}].ingredient`}
-                          ref={register({required: true})}
-                          defaultValue={ingredient.ingredient}
-                          maxLength='150'
-                        />
+            {!fields.length ? 'Ingredients' : (
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th>Ingredient</th>
+                            <th>Amount</th>
+                            <th>Unit</th>
+                            <th></th>
+                        </tr>
+                    </thead>
 
-                        <input
-                          name={`ingredients[${index}].amount`}
-                          type='number'
-                          step='0.1'
-                          max='99999'
-                          min='0' // Max and min used to control input width
-                          ref={register({required: true})}
-                          defaultValue={ingredient.amount}
-                        />
+                    <tbody>
+                        {fields.map((ingredient, index) => (
+                            <tr key={ingredient.id}>
+                                <td>
+                                    <input
+                                      className='ingredients-input'
+                                      name={`ingredients[${index}].ingredient`}
+                                      ref={register({required: true})}
+                                      defaultValue={ingredient.ingredient}
+                                      maxLength='150'
+                                    />
+                                </td>
 
-                        <select
-                          name={`ingredients[${index}].unit`}
-                          ref={register({required: true})}
-                        >
-                          {units.map(unit => (
-                            <option
-                              key={`ingredient-${index}-${unit.value}`}
-                              value={unit.value}
-                            >
-                              {unit.display_name}
-                            </option>
-                          ))}
-                        </select>
+                                <td>
+                                    <input
+                                      className='ingredients-input'
+                                      name={`ingredients[${index}].amount`}
+                                      type='number'
+                                      step='0.1'
+                                      max='99999'
+                                      min='0' // Max and min used to control input width
+                                      ref={register({required: true})}
+                                      defaultValue={ingredient.amount}
+                                    />
+                                </td>
 
-                        <button type='button' onClick={() => remove(index)}>
-                            Remove
-                        </button>
-                      </li>
-                    ))}
+                                <td>
+                                    <select
+                                      className='ingredients-input'
+                                      name={`ingredients[${index}].unit`}
+                                      ref={register({required: true})}
+                                    >
+                                      {units.map(unit => (
+                                        <option
+                                          key={`ingredient-${index}-${unit.value}`}
+                                          value={unit.value}
+                                        >
+                                          {unit.display_name}
+                                        </option>
+                                      ))}
+                                    </select>
+                                </td>
 
-                    <li>
-                        <button
-                            type='button'
-                            onClick={() => append({ ingredient: '', amount: 0, unit: '' })}
-                        >
-                            Add
-                        </button>
-                    </li>
-                </ul>
-            </fieldset>
+                                <td>
+                                    <button
+                                        className='ingredients-input'
+                                        type='button'
+                                        onClick={() => remove(index)}>
+                                        -
+                                    </button>
+                                </td>
+                          </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
+            <button
+                type='button'
+                onClick={() => append({ ingredient: '', amount: 0, unit: '' })}
+            >
+                New Ingredient
+            </button>
 
             <label htmlFor='instructions'>Step by step instructions</label>
             <textarea name='instructions' ref={register({required: true})}/>
